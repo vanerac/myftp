24ad7e3f9e84a50331cabf24383797ee0b1a2ba9
Corentin Mas <corentinmas@yahoo.com>
2021-04-13 16:18:37 +0200

    forgot file...

---
+++ src/commands/stor.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** ${FOLDER_NAME}
+** File description:
+** stor.c file
+*/
+
+#include <commands.h>
+
+int stor(session_t *config, char *argument)
+{
+    return 0;
+}
