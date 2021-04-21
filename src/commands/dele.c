2b67efc3e171f4ef609e45860c6af502624491dd
Corentin Mas <corentinmas@yahoo.com>
2021-04-21 15:02:12 +0200

    [Added] DELE & LIST Command

--- src/commands/dele.c
+++ src/commands/dele.c
@@ -5,11 +5,33 @@
 ** dele.c file
 */
 
-#include <commands.h>
+#include "commands.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "sockets.h"
 #include "sessions.h"
 
 int dele(session_t *config, char *argument)
 {
-    // 250 Requested file action okay, completed.
+    // todo check auth
+    char *direction = argument;
+    char *buffer = calloc(strlen(config->working_dir) + strlen(direction) + 2,
+        1);
+
+    strcat(buffer, config->working_dir);
+    if (buffer[strlen(buffer) - 1] != '/')
+        strcat(&buffer[strlen(buffer) - 1], "/");
+    strcat(&buffer[strlen(buffer)], direction);
+
+    if (buffer[strlen(buffer)] != '/')
+        buffer[strlen(buffer)] = '/';
+
+    if (remove(buffer))
+        write_socket(config->ctrl_fd,
+            "550 Requested action not taken. File unavailable (e.g., file not found, no access).");
+    else
+        write_socket(config->ctrl_fd,
+            "250 Requested file action okay, completed.");
     return 0;
 }
--- src/commands/list.c
+++ src/commands/list.c
@@ -5,29 +5,52 @@
 ** list.c file
 */
 
-#include <commands.h>
-#include <sockets.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include "commands.h"
+#include "sockets.h"
 #include "sessions.h"
 
 int list(session_t *config, char *argument)
 {
+    struct stat s;
+    char *direction = argument;
+    char *buffer;
+
+    if (direction) {
+        buffer = calloc(strlen(config->working_dir) + strlen(direction) + 2,
+            1);
+        strcat(buffer, config->working_dir);
+        if (buffer[strlen(buffer) - 1] != '/')
+            strcat(&buffer[strlen(buffer) - 1], "/");
+        strcat(&buffer[strlen(buffer)], direction);
+    } else
+        buffer = strdup(config->working_dir);
+
+    if (stat(buffer, &s)) {
+        write_socket(config->ctrl_fd,
+            "550 Requested action not taken. File unavailable (e.g., file not found, no access).");
+        return 0;
+    }
+
     write_socket(config->ctrl_fd,
         "150 File status okay; about to open data connection.");
-    // transmit on data chanel ??
 
-    // todo fork ??
-    // todo execve ??
-    // todo error management ???
+    char *args[3] = {"ls", buffer, NULL};
+
     int pid = fork();
-    switch(pid) {
+    switch (pid) {
     case -1:
         perror("fork");
         return 1;
     case 0:
-        write_socket(config->data_fd, "lol");
+        dup2(config->data_fd, 1);
+        dup2(config->data_fd, 2);
+        execvp("ls", args);
+        write_socket(config->data_fd, "");
         exit(0);
     default:
         waitpid(pid, NULL, 0);
@@ -35,6 +58,7 @@ int list(session_t *config, char *argument)
         config->data_fd = -1;
     }
 
+    free(buffer);
     write_socket(config->ctrl_fd, "226 Closing data connection.");
     return 0;
 }
